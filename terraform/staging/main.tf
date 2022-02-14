a83732ec8fdb4bbea0f99a3535462531ba10472a
Oleg Kaspersky <kasperskiyoleg@yandex.ru>
2022-02-14 16:48:23 +0300

    test run

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1,7 +1,6 @@
 module "cluster" {
   source = "../modules/cluster"
 
-  gcp_project = "staging-338816"
   network_name   = "staging"
   subnet_range   = "10.0.0.0/24"
   cluster_name   = "staging-cluster"
